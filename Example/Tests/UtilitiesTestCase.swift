fb03d16e0bcad8ced9e1025cf77db3b4ba22c8e0
Ekin Bulut <ekin@whitehorse.technology>
2019-12-13 14:45:59 +0300

    Utilities tests written

--- Example/Tests/UtilitiesTestCase.swift
+++ Example/Tests/UtilitiesTestCase.swift
@@ -7,30 +7,100 @@
 //
 
 import XCTest
+import AdSupport
+import CoreTelephony
+
 @testable import Dengage_Framework
 
 class UtilitiesTestCase: XCTestCase {
-
-    let sut = Utilities.shared
-    let storage = DengageLocalStorage.shared
+    
+    var storageMock : DengageLocalStorageMock =  DengageLocalStorageMock(suitName: "")
+    var ctTelephonyNetworkInfoMock : CTTelephonyNetworkInfoMock = CTTelephonyNetworkInfoMock()
+    var logMock : SDKLoggerMock = SDKLoggerMock()
+    var asIdentifierManagerMock : ASIdentifierManagerMock = ASIdentifierManagerMock()
+    var sut : Utilities = Utilities.init()
     
     override func setUp() {
+        
+        
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        
+        
+        sut = Utilities.init(storage: storageMock,
+                             logger: logMock,
+                             asIdentifierManager : asIdentifierManagerMock,
+                             ctTelephonyNetworkInfo: ctTelephonyNetworkInfoMock)
+        
+        
     }
-
+    
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
+    
     func testIfStorageReturnsNilGetIdentifierForApplication(){
         
         //TODO : mock storage
-        storage.setValueWithKey(value: "", key: "ApplicationIdentifier")
+        
+        
+        //let sut = Utilities.init(storage: storageMock, logger: logMock, asIdentifierManager : asIdentifierManagerMock)
         
         let actual = sut.identifierForApplication()
         
         XCTAssertNotNil(actual)
         
     }
+    
+    func testIfIsAdvertisingTrackingEnabledGenerateAdvertisingId(){
+        
+        let actual = sut.identifierForAdvertising()
+        
+        XCTAssertNotNil(actual)
+    }
+    
+    func testIdentifierForCarrier(){
+        
+        let actual = sut.identifierForCarrier()
+        
+        XCTAssertEqual("1", actual)
+    }
+    
+}
 
+class ASIdentifierManagerMock : ASIdentifierManager{
+    
+    
+}
+
+
+class DengageLocalStorageMock : DengageLocalStorage{
+    
+    var value : String = ""
+    
+    
+    override func setValueWithKey(value: String, key: String) {
+        self.value = value
+    }
+    
+    override func getValueWithKey(key: String) -> String? {
+        return nil
+    }
+}
+
+class SDKLoggerMock : SDKLogger{
+    
+    
+    
+}
+
+class CTTelephonyNetworkInfoMock : CTTelephonyNetworkInfo{
+    
+    
+}
+
+class CTCarrierMock : CTCarrier{
+    
+    
+    
 }
