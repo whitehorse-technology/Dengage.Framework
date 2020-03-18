ee64f0897181e6614122e1fc45d5b819c276c129
Ekin <ekin@whitehorse.technology>
2020-03-18 15:32:06 +0300

    2.3.0-alpa

---
+++ Dengage.Framework/Classes/Constants.swift
@@ -0,0 +1,49 @@
+//
+//  Constants.swift
+//  dengage.ios.sdk
+//
+//  Created by Ekin Bulut on 2.12.2019.
+//  Copyright © 2019 Dengage. All rights reserved.
+//
+
+import Foundation
+
+
+// MARK:- SERVICE URLS
+let SUBSCRIPTION_SERVICE_URL = "https://push.dengage.com/api/device/subscription"
+let OPEN_EVENT_SERVICE_URL = "https://push.dengage.com/api/mobile/open"
+let TRANSACTIONAL_OPEN_SERVICE_URL = "https://push.dengage.com/api/transactional/mobile/open"
+let EVENT_SERVICE_URL = "https://event.dengage.com/api/event"
+
+
+// MARK:- QUEUE PARAMETERS
+let DEVICE_EVENT_QUEUE = "device-event-queue"
+let SUBSCRIPTION_QUEUE = "subscription-queue"
+let QUEUE_LIMIT = 5
+
+// MARK:- SETTINGS
+let SDK_VERSION = "2.3.0"
+let SUIT_NAME = "group.dengage"
+
+let DEFAULT_CARRIER_ID = "1"
+let MESSAGE_SOURCE = "DENGAGE"
+
+
+// MARK:- ACTION TYPES AND CATEGORIES
+let YES_ACTION = "YES_ACTION"
+let NO_ACTION = "NO_ACTION"
+
+let ACCEPT_ACTION = "ACCEPT_ACTION"
+let DECLINE_ACTION = "DECLINE_ACTION"
+
+let CONFIRM_ACTION = "CONFIRM_ACTION"
+let CANCEL_ACTION = "CANCEL_ACTION"
+
+let PREVIOUS_ACTION = "PREVIOUS_ACTION"
+let NEXT_ACTION = "NEXT_ACTION"
+
+let SIMPLE_CATEGORY = "DENGAGE_SIMPLE_CATEGORY"
+let ASK_CATEGORY = "DENGAGE_ASK_CATEGORY"
+let CONFIRM_CATEGORY = "DENGAGE_CONFIRM_CATEGORY"
+let CAROUSEL_CATEGORY = "DENGAGE_CAROUSEL_CATEGORY"
+
