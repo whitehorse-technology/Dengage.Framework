2f26abd12539586c47ec8267dbd69febcdbbb0a0
Ekin <ekin@whitehorse.technology>
2020-03-19 15:50:25 +0300

    v2.3.1
    category registration methods

--- Dengage.Framework/Classes/Constants.swift
+++ Dengage.Framework/Classes/Constants.swift
@@ -22,7 +22,7 @@ let SUBSCRIPTION_QUEUE = "subscription-queue"
 let QUEUE_LIMIT = 5
 
 // MARK:- SETTINGS
-let SDK_VERSION = "2.3.0"
+let SDK_VERSION = "2.3.1"
 let SUIT_NAME = "group.dengage"
 
 let DEFAULT_CARRIER_ID = "1"
