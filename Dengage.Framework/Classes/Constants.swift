757ce611286b554cda5feef7a1aa5fb3b17a133e
Ekin Bulut <ekin@whitehorse.technology>
2020-12-14 21:48:24 +0300

    version 2.5.19

--- Dengage.Framework/Classes/Constants.swift
+++ Dengage.Framework/Classes/Constants.swift
@@ -20,7 +20,7 @@ let SUBSCRIPTION_QUEUE = "subscription-queue"
 let QUEUE_LIMIT = 5
 
 // MARK:- SETTINGS
-let SDK_VERSION = "2.5.18"
+let SDK_VERSION = "2.5.19"
 
 let SUIT_NAME = "group.dengage"
 
