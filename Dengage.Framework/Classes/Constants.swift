86a880069b4265340e7c5ed7e65d6d23d4f9cb81
Nahit Rüştü HEPER <nahitheper@gmail.com>
2021-03-28 13:02:18 +0300

    in app test duration

--- Dengage.Framework/Classes/Constants.swift
+++ Dengage.Framework/Classes/Constants.swift
@@ -19,7 +19,7 @@ let SUBSCRIPTION_QUEUE = "subscription-queue"
 let QUEUE_LIMIT = 5
 
 // MARK:- SETTINGS
-let IN_APP_MESSAGE_FETCH_DURATION:TimeInterval = 3600000
+let IN_APP_MESSAGE_FETCH_DURATION:TimeInterval = 10000//3600000
 let SDK_VERSION = "2.5.23"
 
 let SUIT_NAME = "group.dengage"
