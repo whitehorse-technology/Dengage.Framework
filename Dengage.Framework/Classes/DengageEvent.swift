93bd2a5e9c582179f5dcdafe758bb1997f27e180
Ekin Bulut <ekin@whitehorse.technology>
2021-04-09 11:01:09 +0300

    Update DengageEvent.swift

--- Dengage.Framework/Classes/DengageEvent.swift
+++ Dengage.Framework/Classes/DengageEvent.swift
@@ -219,9 +219,10 @@ public class DengageEvent {
         }
     }
  
-    private func queryStringParser(urlString: String) {
+    private func queryStringParser(urlString: String?) {
 
-        let url = URL(string: urlString)!
+        guard let urlStr = urlString, let url = URL(string: urlStr) else { return }
+//        let url = URL(string: urlString)!
         let components = URLComponents(url: url, resolvingAgainstBaseURL: false)
         
         if let queryItems = components?.queryItems {
