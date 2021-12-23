6f5c0b96d3da5d8963faf117baffa0e1e29e8b3f
MacBook Pro <anawaz.qadir@gmail.com>
2021-12-23 18:04:06 +0500

    fix: urlcomponents intialisation

--- Dengage.Framework/Services/APIRequestExtension.swift
+++ Dengage.Framework/Services/APIRequestExtension.swift
@@ -35,9 +35,7 @@ protocol APIRequest {
 
 extension APIRequest {
     func asURLRequest() -> URLRequest {
-        var urlComps = URLComponents()
-        urlComps.host = baseURL
-        urlComps.path = path
+        var urlComps = URLComponents(string: baseURL + path)!
         urlComps.queryItems = queryParameters.filter { $0.value != nil }
         urlComps.percentEncodedQuery = urlComps.percentEncodedQuery?.replacingOccurrences(of: "+", with: "%2B")
         var urlRequest = URLRequest(url: urlComps.url!)
